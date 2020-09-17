f9e4b3343a5de57c03c1da3160b17787500c33a8
Plasnid <stuart@sackler.ca>
2020-09-17 12:53:20 -0400

    added a function for export as well

--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@
  * * Importing using {} is called destructuring.  we are pulling bestCheese and worstCheese from the 
  * * object being exported from otherFile.js
  */
-import { bestCheese, worstCheese } from './otherFile.js'
+import { bestCheese, worstCheese, getDogYears } from './otherFile.js'
 console.log("borkings to all and to all a good bork!");
 // * In the line below we are using a template literal to use best and worst cheese in our string
 let titleFun = `${bestCheese} and ${worstCheese} are two different cheeses of interesting origin!.`;
 // * Here we are console logging our string from the line above!
 console.log(titleFun);
+console.log(getDogYears(5));
 
 // * In this line we are using querySelector to grab the h1 tag from our DOM
 // * DOM stands for document object model.  The DOM is how javascript interprets html
--- src/otherFile.js
+++ src/otherFile.js
@@ -4,5 +4,9 @@
 let bestCheese = "cheddar";
 let worstCheese = "Pied-de-Vent";
 
+function getDogYears (humanYears) {
+    return humanYears * 7;
+}
+
 // * we are exporting an object from our file that contains only what we want it too!
-export { bestCheese, worstCheese };
+export { bestCheese, worstCheese, getDogYears };
